0caaaa90087b034e2406486fca5c515bc8ef6942
Deyler Alvarez Fernández <70861729+DeylerAF@users.noreply.github.com>
2025-02-11 21:58:09 -0600

    Refactor ChatItem and ChatList components for improved readability and consistency

--- src/app/components/chat-list.tsx
+++ src/app/components/chat-list.tsx
@@ -3,10 +3,10 @@ import BotIcon from "../icons/bot.svg";
 
 import styles from "./home.module.scss";
 import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  OnDragEndResponder,
+    DragDropContext,
+    Droppable,
+    Draggable,
+    OnDragEndResponder,
 } from "@hello-pangea/dnd";
 
 import { useAppConfig, useChatStore } from "../store";
@@ -21,155 +21,165 @@ import { showConfirm } from "./ui-lib";
 import { useMobileScreen } from "../utils";
 
 export function ChatItem(props: {
-  onClick?: () => void;
-  onDelete?: () => void;
-  title: string;
-  count: number;
-  time: string;
-  selected: boolean;
-  id: string;
-  index: number;
-  narrow?: boolean;
-  template: Template;
+    onClick?: () => void;
+    onDelete?: () => void;
+    title: string;
+    count: number;
+    time: string;
+    selected: boolean;
+    id: string;
+    index: number;
+    narrow?: boolean;
+    template: Template;
 }) {
-  const config = useAppConfig();
-  const draggableRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    if (props.selected && draggableRef.current) {
-      draggableRef.current?.scrollIntoView({
-        block: "center",
-      });
-    }
-  }, [props.selected]);
+    const config = useAppConfig();
+    const draggableRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        if (props.selected && draggableRef.current) {
+            draggableRef.current?.scrollIntoView({
+                block: "center",
+            });
+        }
+    }, [props.selected]);
 
-  const { pathname: currentPath } = useLocation();
-  return (
-    <Draggable draggableId={`${props.id}`} index={props.index}>
-      {(provided) => (
-        <div
-          className={`${styles["chat-item"]} ${
-            props.selected &&
-            (currentPath === Path.Chat || currentPath === Path.Home) &&
-            styles["chat-item-selected"]
-          }`}
-          onClick={props.onClick}
-          ref={(ele) => {
-            draggableRef.current = ele;
-            provided.innerRef(ele);
-          }}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          title={`${props.title}\n${Locale.ChatItem.ChatItemCount(
-            props.count,
-          )}`}
-        >
-          {props.narrow ? (
-            <div className={styles["chat-item-narrow"]}>
-              <div className={styles["chat-item-avatar"] + " no-dark"}>
-                <TemplateAvatar
-                  avatar={props.template.avatar}
-                  model={config.modelConfig.model}
-                />
-              </div>
-              <div className={styles["chat-item-narrow-count"]}>
-                {props.count}
-              </div>
-            </div>
-          ) : (
-            <>
-              <div className={styles["chat-item-title"]}>{props.title}</div>
-              <div className={styles["chat-item-info"]}>
-                <div className={styles["chat-item-count"]}>
-                  {Locale.ChatItem.ChatItemCount(props.count)}
+    const { pathname: currentPath } = useLocation();
+    return (
+        <Draggable draggableId={`${props.id}`} index={props.index}>
+            {(provided) => (
+                <div
+                    className={`${styles["chat-item"]} ${
+                        props.selected &&
+                        (currentPath === Path.Chat ||
+                            currentPath === Path.Home) &&
+                        styles["chat-item-selected"]
+                    }`}
+                    onClick={props.onClick}
+                    ref={(ele) => {
+                        draggableRef.current = ele;
+                        provided.innerRef(ele);
+                    }}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    title={`${props.title}\n${Locale.ChatItem.ChatItemCount(
+                        props.count
+                    )}`}
+                >
+                    {props.narrow ? (
+                        <div className={styles["chat-item-narrow"]}>
+                            <div
+                                className={
+                                    styles["chat-item-avatar"] + " no-dark"
+                                }
+                            >
+                                <TemplateAvatar
+                                    avatar={props.template.avatar}
+                                    model={config.modelConfig.model}
+                                />
+                            </div>
+                            <div className={styles["chat-item-narrow-count"]}>
+                                {props.count}
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <div className={styles["chat-item-title"]}>
+                                {props.title}
+                            </div>
+                            <div className={styles["chat-item-info"]}>
+                                <div className={styles["chat-item-count"]}>
+                                    {Locale.ChatItem.ChatItemCount(props.count)}
+                                </div>
+                                <div className={styles["chat-item-date"]}>
+                                    {props.time}
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    <div
+                        className={styles["chat-item-delete"]}
+                        onClickCapture={(e) => {
+                            props.onDelete?.();
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }}
+                    >
+                        <DeleteIcon />
+                    </div>
                 </div>
-                <div className={styles["chat-item-date"]}>{props.time}</div>
-              </div>
-            </>
-          )}
-
-          <div
-            className={styles["chat-item-delete"]}
-            onClickCapture={(e) => {
-              props.onDelete?.();
-              e.preventDefault();
-              e.stopPropagation();
-            }}
-          >
-            <DeleteIcon />
-          </div>
-        </div>
-      )}
-    </Draggable>
-  );
+            )}
+        </Draggable>
+    );
 }
 
 export function ChatList(props: { narrow?: boolean }) {
-  const [sessions, selectedIndex, selectSession, moveSession] = useChatStore(
-    (state) => [
-      state.sessions,
-      state.currentSessionIndex,
-      state.selectSession,
-      state.moveSession,
-    ],
-  );
-  const chatStore = useChatStore();
-  const navigate = useNavigate();
-  const isMobileScreen = useMobileScreen();
+    const sessions = useChatStore((state) => state.sessions);
+    const selectedIndex = useChatStore((state) => state.currentSessionIndex);
+    const selectSession = useChatStore((state) => state.selectSession);
+    const moveSession = useChatStore((state) => state.moveSession);
+    const deleteSession = useChatStore((state) => state.deleteSession);
 
-  const onDragEnd: OnDragEndResponder = (result) => {
-    const { destination, source } = result;
-    if (!destination) {
-      return;
-    }
+    const navigate = useNavigate();
+    const isMobileScreen = useMobileScreen();
 
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
+    const onDragEnd: OnDragEndResponder = (result) => {
+        const { destination, source } = result;
+        if (!destination) {
+            return;
+        }
 
-    moveSession(source.index, destination.index);
-  };
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
 
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="chat-list">
-        {(provided) => (
-          <div
-            className={styles["chat-list"]}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {sessions.map((item, i) => (
-              <ChatItem
-                title={item.topic}
-                time={new Date(item.lastUpdate).toLocaleString()}
-                count={item.messages.length}
-                key={item.id}
-                id={item.id}
-                index={i}
-                selected={i === selectedIndex}
-                onClick={() => {
-                  navigate(Path.Chat);
-                  selectSession(i);
-                }}
-                onDelete={async () => {
-                  if (
-                    (!props.narrow && !isMobileScreen) ||
-                    (await showConfirm(Locale.Home.DeleteChat))
-                  ) {
-                    chatStore.deleteSession(i);
-                  }
-                }}
-                narrow={props.narrow}
-                template={item.template}
-              />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+        moveSession(source.index, destination.index);
+    };
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="chat-list">
+                {(provided) => (
+                    <div
+                        className={styles["chat-list"]}
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        {sessions.map((item, i) => (
+                            <ChatItem
+                                title={item.topic}
+                                time={new Date(
+                                    item.lastUpdate
+                                ).toLocaleString()}
+                                count={item.messages.length}
+                                key={item.id}
+                                id={item.id}
+                                index={i}
+                                selected={i === selectedIndex}
+                                onClick={() => {
+                                    navigate(Path.Chat);
+                                    selectSession(i);
+                                }}
+                                onDelete={async () => {
+                                    if (
+                                        (!props.narrow && !isMobileScreen) ||
+                                        (await showConfirm(
+                                            Locale.Home.DeleteChat
+                                        ))
+                                    ) {
+                                        chatStore.deleteSession(i);
+                                    }
+                                }}
+                                narrow={props.narrow}
+                                template={item.template}
+                            />
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
 }
